12bf407604800806ae848a805a8edc820b9e9cd8
Artsiom Tratsiuk <inqui05@gmail.com>
2020-11-26 20:23:15 +0300

    fix closure

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 'use strict';
-let number = Math.floor(Math.random() * 100) + 1;
 let text = 'Угадай число от 1 до 100';
+
+function startGame(){
+    let number = Math.floor(Math.random() * 100) + 1;
+    askPersonNumber(text, number);
+}
 
 function isNumber( data ){
     return !isNaN( parseFloat( data )) && isFinite( data );
 }
 
-function askPersonNumber(text){
+function askPersonNumber(text, number){
     let ask = prompt(text); 
     console.log(ask);
     if (ask === null){
         alert('Игра окончена!');
     } else if (!isNumber(ask)){
-        askPersonNumber('Введи число!');
+        askPersonNumber('Введи число!', number);
     } else {
         if(+ask === number){
             alert('Поздравляю, Вы угадали!!!');
         } else if (+ask > number){
             alert('Загаданное число меньше');
-            askPersonNumber('Ещё попыточка?');
+            askPersonNumber('Ещё попыточка?', number);
         } else {
             alert('Загаданное число больше');
-            askPersonNumber('Ещё попыточка?');
+            askPersonNumber('Ещё попыточка?', number);
         }
     }
 }
 
-askPersonNumber(text);
+startGame();
