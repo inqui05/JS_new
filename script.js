4e26a27a5ece0137710fea0b71fca3e2d888b041
Artsiom Tratsiuk <inqui05@gmail.com>
2020-11-26 15:27:53 +0300

    my homework to the lesson06

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+let number = Math.floor(Math.random() * 100) + 1;
+let text = 'Угадай число от 1 до 100';
+
+function isNumber( data ){
+    return !isNaN( parseFloat( data )) && isFinite( data );
+}
+
+function askPersonNumber(text){
+    let ask = prompt(text); 
+    console.log(ask);
+    if (ask === null){
+        alert('Игра окончена!');
+    } else if (!isNumber(ask)){
+        askPersonNumber('Введи число!');
+    } else {
+        if(+ask === number){
+            alert('Поздравляю, Вы угадали!!!');
+        } else if (+ask > number){
+            alert('Загаданное число меньше');
+            askPersonNumber('Ещё попыточка?');
+        } else {
+            alert('Загаданное число больше');
+            askPersonNumber('Ещё попыточка?');
+        }
+    }
+}
+
+askPersonNumber(text);
