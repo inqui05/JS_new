eda75d8a6569c4272f67eb04e5afdc46595a9097
Artsiom Tratsiuk <inqui05@gmail.com>
2020-12-02 21:55:49 +0300

    my homework to lesson10

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 'use strict';
-let text = 'Угадай число от 1 до 100';
+let books = document.querySelectorAll('.book'),
+    elements = document.querySelectorAll('li');
 
-function startGame(){
-    let number = Math.floor(Math.random() * 100) + 1;
-    askPersonNumber(text, number);
-}
+const newChapter = document.createElement('li'),
+    body = document.querySelector('body'),
+    advertising = document.querySelector('.adv');
 
-function isNumber( data ){
-    return !isNaN( parseFloat( data )) && isFinite( data );
-}
+books[0].before(books[1]);
+books[5].after(books[2]);
+books[4].after(books[3]);
 
-function askPersonNumber(text, number){
-    let ask = prompt(text); 
-    console.log(ask);
-    if (ask === null){
-        alert('Игра окончена!');
-    } else if (!isNumber(ask)){
-        askPersonNumber('Введи число!', number);
-    } else {
-        if(+ask === number){
-            alert('Поздравляю, Вы угадали!!!');
-        } else if (+ask > number){
-            alert('Загаданное число меньше');
-            askPersonNumber('Ещё попыточка?', number);
-        } else {
-            alert('Загаданное число больше');
-            askPersonNumber('Ещё попыточка?', number);
-        }
-    }
-}
+//меняем фоновую картинку
+document.body.style.backgroundImage = "url('http://127.0.0.1:5500/image/you-dont-know-js.jpg')";
 
-startGame();
+//изменяем название книги
+books[4].innerHTML =  books[4].innerHTML.replace('Книга 3. this и Пропопипы Объектов', 'Книга 3. this и Прототипы Объектов');
+
+//удаляем рекламу
+advertising.remove();
+
+//восставливаем порядок глав
+elements[9].after(elements[2]);
+elements[4].before(elements[8]);
+elements[3].after(elements[6]);
+elements[47].after(elements[55]);
+elements[54].before(elements[51]);
+elements[52].before(elements[48]);
+
+//вставляем дополнительную главу в книгу 6
+newChapter.textContent = 'Глава 8: За пределами ES6';
+elements[25].after(newChapter);
