8a317dd50e8ae4d06737535466b9ba783cd58cd1
Artsiom Tratsiuk <inqui05@gmail.com>
2020-12-09 20:58:07 +0300

    fix constructor

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 'use strict';
 let elementOnPage = document.querySelector('.lesson14');
 
-const DomElement = function(){
-    this.selector = 0;
-    this.height = '150px';
-    this.width = '800px';
-    this.bg = 'RGB(249, 201, 16)';
-    this.fontSize = '100px';
-    this.creatElement = function(selector){
+const DomElement = function(selector, height, width, bg, fontSize){
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.creatElement = function(){
         let elem;
-        if (selector[0] === '#'){
+        if (this.selector[0] === '#'){
             elem = document.createElement("p");
-            elem.setAttribute('id',selector.substr(1));
-            elem.style.cssText = `height: ${this.height};
-                background-color: ${this.bg};
-                width: ${this.width};
-                font-size: ${this.fontSize}`;
             elem.innerHTML = 'Это параграф p!';
-            elementOnPage.after(elem);
-        } else if (selector[0] === '.'){
+        } else if (this.selector[0] === '.'){
             elem = document.createElement("div");
-            elem.className = selector.substr(1);
-            elem.style.cssText = `height: ${this.height};
-                background-color: ${this.bg};
-                width: ${this.width};
-                font-size: ${this.fontSize}`;
             elem.innerHTML = '<strong>А вот и блок div!</strong>';
-            elementOnPage.after(elem);
         }
+        elem.setAttribute('id',selector.substr(1));
+        elem.style.cssText = `height: ${this.height};
+            background-color: ${this.bg};
+            width: ${this.width};
+            font-size: ${this.fontSize}`;
+            elementOnPage.after(elem);
     };
 };
 
-let domElement = new DomElement();
+let domElement = new DomElement('#lol', '150px', '800px', 'RGB(249, 201, 16)', '100px');
 
-domElement.creatElement('.lol');
+domElement.creatElement();
